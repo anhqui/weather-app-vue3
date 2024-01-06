2c0e2d98c181257e1ac9ef2d5f93558a48bc265b
Qui Nguyen <qui@gmail.com>
2024-01-06 17:03:42 -0500

    first commit

---
+++ src/components/CityList.vue
@@ -0,0 +1,46 @@
+<template>
+    <div v-for="city in savedCities" :key="city.id">
+        <CityCard :city="city" @click="goToCityView(city)"/>
+    </div>
+</template>
+
+<script setup>
+import axios from "axios"
+import {ref} from "vue"
+import { useRouter } from "vue-router"
+import CityCard from "./CityCard.vue"
+
+const savedCities = ref([])
+const getCities = async () =>{
+    if(localStorage.getItem("savedCities")){
+        savedCities.value = JSON.parse(localStorage.getItem("savedCities"))
+
+        const requests = [];
+savedCities.value.forEach(city =>{
+    requests.push(axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${city.coords.lat}&lon=${city.coords.lng}&appid=a65c9b4bae2fd83474fa14a790dda27e&units=imperial`))
+}) 
+
+const weatherData = await Promise.all(requests)
+
+// Flicker Delay
+await new Promise(res => setTimeout(res, 1000))
+
+
+weatherData.forEach((value, index) =>{
+    savedCities.value[index].weather = value.data;
+})
+}
+}
+
+await getCities()
+const router = useRouter();
+const goToCityView = (city) =>{
+  router.push({
+    name: "cityView",
+    params: {state: city.state, city: city.city},
+    query: {id: city.id, lat: city.coords.lat, lng: city.coords.lng}
+  })
+}
+
+</script>
+
